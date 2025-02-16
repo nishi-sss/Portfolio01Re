edd609494c0d29c90d36c8ddeb85bdc833b2f443
nishi <return.0return.1@gmail.com>
2025-02-17 04:02:10 +0900

    backend update heroku

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,6 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Herokuの$PORTを取得
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))  # ← ここが間違ってたら RIP😇
+    app.run(debug=False, host="0.0.0.0", port=port)  # debug=False にして負荷軽減！
 
