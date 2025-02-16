c94a51bfb03ada1b08b1d12b1659f40a6926feec
nishi <return.0return.1@gmail.com>
2025-02-17 03:43:09 +0900

    backend update

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,11 +1,9 @@
-# WSGIエントリポイント
-# Heroku用のアプリエントリポイント。
-
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))  # Herokuの$PORTを取得
+    app.run(debug=True, host="0.0.0.0", port=port)
 
